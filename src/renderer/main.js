6f746f0f4dbb39ed5338426aa0f5bbb052c895b9
Wenish <Jonas.Voland@swisscom.com>
2018-07-19 16:41:03 +0200

    added - vue material - vuex router sync - page home

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
 import axios from 'axios'
+import { sync } from 'vuex-router-sync';
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
 import App from './App'
 import router from './router'
 import store from './store'
 
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.use(VueMaterial)
+
 Vue.http = Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
+
+// Sync the router with the vuex store. This registers `store.state.route`
+sync(store, router);
 
 /* eslint-disable no-new */
 new Vue({
