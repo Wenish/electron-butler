ec44c4e026a3a6bc025bc461ef262b829927765a
Wenish <Wenish@users.noreply.github.com>
2018-07-21 02:05:14 +0200

    changed webchat style

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -11,5 +11,11 @@
 </script>
 
 <style>
-  /* CSS */
+  body{
+    /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#cfe4f7+0,e5f0f9+100 */
+    background: #cfe4f7; /* Old browsers */
+    background: -moz-linear-gradient(-45deg, #cfe4f7 0%, #e5f0f9 100%); /* FF3.6-15 */
+    background: -webkit-linear-gradient(-45deg, #cfe4f7 0%,#e5f0f9 100%); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(135deg, #cfe4f7 0%,#e5f0f9 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#cfe4f7', endColorstr='#e5f0f9',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */}
 </style>
