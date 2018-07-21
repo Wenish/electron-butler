3313bcea0296e9e7d490fe5e5e54f0b3e4498d61
Wenish <jonas.voland@bluewin.ch>
2018-07-21 19:12:40 +0200

    added basic app layout

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,7 @@ import { sync } from 'vuex-router-sync';
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 import App from './App'
 import router from './router'
